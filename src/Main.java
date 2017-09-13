d242b8810754b7d0f19f60d139425edf4fbfc89e
ajaydevgan <31915322+ajaydevgan@users.noreply.github.com>
2017-09-13 12:57:55 +0530

    submit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		/*Student s=new Student(583,"Ajay kumar",1996-02-20,90.2);
+		Student s=new Student(561,"vinay pokar",1997-01-20,80.7);
+		Student s=new Student(590,"vignan",1996-05-20,69.2);*/
+	}
+
+}
